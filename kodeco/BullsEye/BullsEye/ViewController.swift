dcfad38c7584b17b791f5847c6c74b3cc8db9e98
jessicavsampaio <jessicavsampaio@gmail.com>
2024-05-16 11:01:49 -0300

    excluindo DS_Store

---
+++ kodeco/BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Jessica Sampaio on 15/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: "This is my first app!",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+    }
+}
+
